ada8e2e1dcdd80f9e53cd85e0060913e6bade4ca
bobish <bobishaugust@gmail.com>
2024-04-19 08:22:34 +0530

    message

--- FL/BAKER.php
+++ FL/BAKER.php
@@ -47,7 +47,7 @@ try {
 	
 	echo $app_name." : ".$batchDetails->county."\n";
 
-	$url = "http://208.75.175.18/landmarkwe";
+	$url = "http://208.75.175.18/landmarkweb";
 	$driver->get($url);
 		
 	$driver->wait()->until(
